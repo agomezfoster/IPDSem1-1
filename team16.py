167a3566724cc9d88c5e2c18c5780b0b35bd0ea4
agomezfoster <57813353+agomezfoster@users.noreply.github.com>
2019-11-22 14:15:13 -0600

    finished code

--- team16.py
+++ team16.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'team 16' # Only 10 chars displayed.
+strategy_name = 'check history'
+strategy_description = 'it will look at the histoy and do the opposite '
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +17,37 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if len(my_history) == 0:
+        return 'c'
+    if (their_history[-1]) == 'b':
+        return 'b'
+    else:
+        return 'c'
+        
+    if len(their_history) == 10:
+        their_history == 'c'
+        return 'b' 
+    else:
+        return 'c'
+        
+    if their_score is 0>-100 :
+        return 'b'
+    else:
+        return 'c'
+        
+    if (my_score) is 0>-100:
+        return 'c'
+    else:
+        return 'b'
+    
+    if(their_history) == 20:
+        their_history =='c'
+        return 'c'
+    else:
+        return 'b'
 
+         
+         
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -53,7 +83,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print 'Test passed'
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
